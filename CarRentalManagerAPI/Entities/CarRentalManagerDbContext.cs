e36a118981bd195a3f5b1d80a4dbdf2971fe5439
Adam Cieslak <adamcieslak00@gmail.com>
2023-08-03 11:52:33 +0200

    Refactor DbContext

--- CarRentalManagerAPI/Entities/CarRentalManagerDbContext.cs
+++ CarRentalManagerAPI/Entities/CarRentalManagerDbContext.cs
@@ -15,110 +15,48 @@ namespace CarRentalManagerAPI.Entities
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            //Car
-            modelBuilder.Entity<Car>()
-                .Property(c => c.Status)
-                .IsRequired();
-            modelBuilder.Entity<Car>()
-                .Property(c => c.Comments)
-                .HasMaxLength(50);
-            modelBuilder.Entity<Car>()
-                .Property(c => c.Transmission)
-                .IsRequired();
-            modelBuilder.Entity<Car>()
-                .Property(c => c.Mark)
-                .HasMaxLength(25)
-                .IsRequired();
-            modelBuilder.Entity<Car>()
-                .Property(c => c.DrivingLicenseCategory)
-                .IsRequired();
-            modelBuilder.Entity<Car>()
-                .Property(c => c.EnginePower)
-                .HasMaxLength(4)
-                .IsRequired();
-            modelBuilder.Entity<Car>()
-                .Property(c => c.Model)
-                .HasMaxLength(25)
-                .IsRequired();
-            modelBuilder.Entity<Car>()
-                .Property(c => c.NumberOfSeats)
-                .HasMaxLength(2)
-                .IsRequired();
-            modelBuilder.Entity<Car>()
-                .Property(c => c.PricePerDay)
-                .HasMaxLength(6)
-                .IsRequired();
-            modelBuilder.Entity<Car>()
-                .Property(c => c.RegistrationNumber)
-                .HasMaxLength(10)
-                .IsRequired();
-            modelBuilder.Entity<Car>()
-                .Property(c => c.VIN)
-                .HasMaxLength(25)
-                .IsRequired();
+            modelBuilder.Entity<Car>(eb =>
+            {
+                eb.Property(c => c.Status).IsRequired();
+                eb.Property(c => c.Transmission).IsRequired();
+                eb.Property(c => c.DrivingLicenseCategory).IsRequired();
+                eb.Property(c => c.Comments).HasMaxLength(50);
+                eb.Property(c => c.Mark).HasMaxLength(25).IsRequired();
+                eb.Property(c => c.EnginePower).HasMaxLength(4).IsRequired();
+                eb.Property(c => c.Model).HasMaxLength(25).IsRequired();
+                eb.Property(c => c.NumberOfSeats).HasMaxLength(2).IsRequired();
+                eb.Property(c => c.PricePerDay).HasMaxLength(6).IsRequired();
+                eb.Property(c => c.RegistrationNumber).HasMaxLength(10).IsRequired();
+                eb.Property(c => c.VIN).HasMaxLength(25).IsRequired();
+            });
 
-            //Client
-            modelBuilder.Entity<Client>()
-                .Property(c=>c.Surname)
-                .HasMaxLength(25)
-                .IsRequired();
-            modelBuilder.Entity<Client>()
-                .Property(c => c.IsBlocked)
-                .IsRequired();
-            modelBuilder.Entity<Client>()
-                .Property(c => c.Comments)
-                .HasMaxLength(50);
-            modelBuilder.Entity<Client>()
-                .Property(c => c.drivingLicenseCategories)
-                .IsRequired();
-            modelBuilder.Entity<Client>()
-                .Property(c => c.Email)
-                .HasMaxLength(25)
-                .IsRequired();
-            modelBuilder.Entity<Client>()
-                .Property(c => c.Name)
-                .HasMaxLength(25)
-                .IsRequired();
-            modelBuilder.Entity<Client>()
-                .Property(c => c.PESELOrPassportNumber)
-                .HasMaxLength(25)
-                .IsRequired();
-            modelBuilder.Entity<Client>()
-                .Property(c => c.PhoneNumber)
-                .HasMaxLength(9)
-                .IsRequired();
+            modelBuilder.Entity<Client>(eb =>
+            {
+                eb.Property(c => c.IsBlocked).IsRequired();
+                eb.Property(c => c.drivingLicenseCategories).IsRequired();
+                eb.Property(c => c.Comments).HasMaxLength(50);
+                eb.Property(c => c.Surname).HasMaxLength(25).IsRequired();
+                eb.Property(c => c.Email).HasMaxLength(25).IsRequired();
+                eb.Property(c => c.Name).HasMaxLength(25).IsRequired();
+                eb.Property(c => c.PESELOrPassportNumber).HasMaxLength(25).IsRequired();
+                eb.Property(c => c.PhoneNumber).HasMaxLength(9).IsRequired();
+            });
 
-            //Rental
-            modelBuilder.Entity<Rental>()
-                .Property(r => r.Status)
-                .IsRequired();
-            modelBuilder.Entity<Rental>()
-                .Property(r => r.Amount)
-                .HasMaxLength(25)
-                .IsRequired();
-            modelBuilder.Entity<Rental>()
-                .Property(r => r.Comments)
-                .HasMaxLength(50);
-            modelBuilder.Entity<Rental>()
-                .Property(r => r.ExpectedDateOfReturn)
-                .IsRequired();
-            modelBuilder.Entity<Rental>()
-                .Property(r => r.RentalDate)
-                .IsRequired();
+            modelBuilder.Entity<Rental>(eb =>
+            {
+                eb.Property(r => r.Status).IsRequired();
+                eb.Property(r => r.ExpectedDateOfReturn).IsRequired();
+                eb.Property(r => r.RentalDate).IsRequired();
+                eb.Property(r => r.Comments).HasMaxLength(50);
+                eb.Property(r => r.Amount).HasMaxLength(25).IsRequired();
+            });
 
-            //User
-            modelBuilder.Entity<User>()
-                .Property(u => u.Surname)
-                .HasMaxLength(25)
-                .IsRequired();
-            modelBuilder.Entity<User>()
-                .Property(u => u.Name)
-                .HasMaxLength(25)
-                .IsRequired();
-            modelBuilder.Entity<User>()
-                .Property(u => u.PhoneNumber)
-                .HasMaxLength(9)
-                .IsRequired();
+            modelBuilder.Entity<User>(eb =>
+            {
+                eb.Property(u => u.Surname).HasMaxLength(25).IsRequired();
+                eb.Property(u => u.Name).HasMaxLength(25).IsRequired();
+                eb.Property(u => u.PhoneNumber).HasMaxLength(9).IsRequired();
+            });
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
---
+++ CarRentalManagerAPI/Migrations/20230803095133_Refactor3.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarRentalManagerAPI.Migrations
+{
+    public partial class Refactor3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
