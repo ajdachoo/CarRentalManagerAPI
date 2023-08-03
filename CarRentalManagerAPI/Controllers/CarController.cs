4379cc4dbab6d143850fbbd8943749d7f4bac14b
Adam Cieslak <adamcieslak00@gmail.com>
2023-08-03 14:59:02 +0200

    Add CarController

---
+++ CarRentalManagerAPI/Controllers/CarController.cs
@@ -0,0 +1,38 @@
+﻿using CarRentalManagerAPI.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CarRentalManagerAPI.Controllers
+{
+    [Route("api/cars")]
+    public class CarController : ControllerBase
+    {
+        private readonly CarRentalManagerDbContext _dbContext;
+
+        public CarController(CarRentalManagerDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Car>> GetAll()
+        {
+            var cars = _dbContext.Cars.ToList();
+            return Ok(cars);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Car> Get([FromRoute] int id)
+        {
+            var car = _dbContext.Cars.FirstOrDefault(c => c.Id == id);
+
+            if(car is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(car);
+        }
+    }
+}
